387922453b216b7e33fd0329c4fb96d8869a5a92
sonb9615 <sonb9615@gmail.com>
2024-03-03 22:35:03 +0900

    feat: 공통 annotation 생성

---
+++ src/main/java/com/practice/hexagonal/common/PersistenceAdaptor.java
@@ -0,0 +1,16 @@
+package com.practice.hexagonal.common;
+
+import org.springframework.core.annotation.AliasFor;
+import org.springframework.stereotype.Component;
+
+import java.lang.annotation.*;
+
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Component
+public @interface PersistenceAdaptor {
+
+    @AliasFor(annotation = Component.class)
+    String value() default "";
+}
