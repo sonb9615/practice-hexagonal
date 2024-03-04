dcdbc6536b2d996def1b871a748c7a921067d8d7
sonb9615 <sonb9615@gmail.com>
2024-03-05 00:27:02 +0900

    feat: 쿠폰 발행 서비스 테스트

---
+++ src/test/java/com/practice/hexagonal/coupon/application/service/IssueCouponServiceTest.java
@@ -0,0 +1,25 @@
+package com.practice.hexagonal.coupon.application.service;
+
+import com.practice.hexagonal.coupon.application.port.in.IssueCouponCommand;
+import com.practice.hexagonal.coupon.application.port.out.IssueCouponPort;
+import com.practice.hexagonal.coupon.domain.MembershipPlan.MembershipPlanId;
+import com.practice.hexagonal.coupon.domain.User.IssuerUserId;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+public class IssueCouponServiceTest {
+
+    private final IssueCouponPort issueCouponPort = Mockito.mock(IssueCouponPort.class);
+    private final IssueCouponService issueCouponService = new IssueCouponService(issueCouponPort);
+
+    @Test
+    void 쿠폰_발행_정상_작동() {
+        MembershipPlanId membershipPlanId = new MembershipPlanId(1L);
+        IssuerUserId issuerUserId = new IssuerUserId(1L);
+        IssueCouponCommand issueCouponCommand = new IssueCouponCommand(membershipPlanId, issuerUserId);
+
+        issueCouponService.issueCoupon(issueCouponCommand);
+    }
+
+
+}
